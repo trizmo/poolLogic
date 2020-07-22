f4634d04c98a290abe4115a3dfee2b46ef7fb25c
Tristan Perera <tristan.e.perera@gmail.com>
2020-07-21 21:38:33 -0700

    more calculating

--- client/src/components/root/totals/Totals.js
+++ client/src/components/root/totals/Totals.js
@@ -7,8 +7,12 @@ export default class Totals extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      // GENERAL
       plans: 0,
       engineering: 0,
+      generalTotal: 0,
+
+      // EXCAVATION
       excavationZone: 0,
       excavationDemo: 0,
       excavationSod: 0,
@@ -18,11 +22,26 @@ export default class Totals extends Component {
       excavationRockArea: 0,
       excavationDayOfDig: 0,
       excavationSteelSquareFoot: 0,
-      generalTotal: 0,
       excavationTotal: 0,
+
+      // PLUMBING AND ELECTRICAL
+      plumbingType: 0,
+      plumbingLength: 0,
+      gasLength: 0,
+      electricalLength: 0,
+      stubsOutlets: 0,
+      dedicatedSuction: 0,
+      bondingWire: 0,
+      sawCut: 0,
       plumbElectTotal: 0,
+
+      // SHOTCRETE TOTAL
       shotcreteTotal: 0,
+
+      // MASONRY TOTAL
       masonryTotal: 0,
+
+      // ADDITIONAL TOTAL
       additionsTotal: 0,
       grandTotal: 0,
     };
@@ -34,6 +53,8 @@ export default class Totals extends Component {
     console.log("getDerivedStateFromProps: ", props.allData.engineering)
     let update = {}
 
+
+    // GENERAL
     if (props.allData.plans) {
       update.plans = 400
     }
@@ -48,6 +69,7 @@ export default class Totals extends Component {
       update.engineering = 0
     }
 
+    // EXCAVATION
     if (props.allData.excavationZone === "riversideCounty") {
       update.excavationZone = 1000
     }
@@ -122,14 +144,95 @@ export default class Totals extends Component {
       }
     }
 
+    // PLUMBING AND ELECTRICAL
+    if (props.allData.plumbingType && props.allData.plumbingLength) {
+      if (props.allData.plumbingType === "poolOnly") {
+        update.plumbingLength = 3000 + (30 * props.allData.plumbingLength)
+      }
+      else if (props.allData.plumbingType === "poolAndSpa") {
+        update.plumbingLength = 4000 + (30 * props.allData.plumbingLength)
+      }
+      else if (props.allData.plumbingType === "PoolAndSpaRemodel") {
+        update.plumbingLength = 5000 + (0 * props.allData.plumbingLength)
+      }
+      else {
+        update.plumbingLength = 0
+      }
+    }
+
+    if (props.allData.gasLength) {
+      if (props.allData.gasLength < 40) {
+        update.gasLength = 450
+
+      } else if (props.allData.gasLength >= 40) {
+        update.gasLength = (8 * props.allData.gasLength) + 450
+      }
+      else {
+        update.gasLength = 0
+      }
+    }
+
+    if (props.allData.gasLength) {
+      if (props.allData.gasLength < 40) {
+        update.gasLength = 450
+
+      } else if (props.allData.gasLength >= 40) {
+        update.gasLength = (8 * props.allData.gasLength) + 450
+      }
+      else {
+        update.gasLength = 0
+      }
+    }
+
+    if (props.allData.electricalLength) {
+      if (props.allData.electricalLength < 40) {
+        update.electricalLength = 1150
+
+      } else if (props.allData.electricalLength >= 40) {
+        update.electricalLength = (8 * props.allData.electricalLength) + 1150
+      }
+      else {
+        update.electricalLength = 0
+      }
+    }
+
+    if (props.allData.stubsOutlets) {
+      update.stubsOutlets = 175 * props.allData.stubsOutlets
+    }
+    else {
+      update.stubsOutlets = 0
+    }
+
+    if (props.allData.dedicatedSuction) {
+      update.dedicatedSuction = 250
+    }
+    else {
+      update.dedicatedSuction = 0
+    }
+
+    if (props.allData.bondingWire) {
+      update.bondingWire = 200
+    }
+    else {
+      update.bondingWire = 0
+    }
+
+    if (props.allData.sawCut) {
+      update.sawCut = 350
+    }
+    else {
+      update.sawCut = 0
+    }
+
 
 
 
     return update;
   }
 
+  // UPDATING TOTALS
   componentDidUpdate(prevProps, prevState) {
-    console.log("componentDidUpdate")
+    // GENERAL
     let general = this.state.plans + this.state.engineering
     if (general !== this.state.generalTotal) {
       this.setState({
@@ -137,14 +240,29 @@ export default class Totals extends Component {
       })
     }
 
+    // EXCAVATION
     let excavation = this.state.excavationZone + this.state.excavationDemo + this.state.excavationSod + this.state.excavationConcreteDemo + this.state.excavationGrading + this.state.excavationDeepRamp + this.state.excavationRockArea + this.state.excavationDayOfDig + this.state.excavationSteelSquareFoot
-
     if (excavation !== this.state.excavationTotal) {
       this.setState({
         excavationTotal: excavation
       })
     }
 
+    // PLUMBING AND ELECTRICAL
+    let plumbElect = this.state.plumbingLength + this.state.gasLength + this.state.electricalLength + this.state.stubsOutlets + this.state.dedicatedSuction + this.state.bondingWire + this.state.sawCut
+    if (plumbElect !== this.state.plumbElectTotal) {
+      this.setState({
+        plumbElectTotal: plumbElect
+      })
+    }
+
+    // GRAND TOTAL
+    let grand = this.state.generalTotal + this.state.excavationTotal + this.state.plumbElectTotal
+    if (grand !== this.state.grandTotal) {
+      this.setState({
+        grandTotal: grand
+      })
+    }
 
   }
 
